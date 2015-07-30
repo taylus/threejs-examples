9b9c429ad30ca1b6527d874bfea6f7a950896365
Taylus <dragondude@gmail.com>
2015-07-30 00:26:35 -0500

    Adding camera controls and a resize handler

--- 01 - basic cube/cube.js
+++ 01 - basic cube/cube.js
@@ -6,12 +6,16 @@
     
     //make a camera with an FOV of 75, an aspect ratio matching this window, and clipping planes at these values
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 2;
     
     //make a renderer with 1:1 resolution and attach its canvas to the body of the page
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(0x2c2c2c, 1);
     document.body.appendChild(renderer.domElement);
+    
+    //add camera + touch camera controls to the canvas
+    var controls = new THREE.OrbitControls(camera, render.domElement);
     
     //make a cube with a different color on every face
     var geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -24,17 +28,21 @@
         new THREE.MeshBasicMaterial({color: 0xff0000})
     ]);
     var cube = new THREE.Mesh(geometry, materials);
-    
-    //move it back a bit (so the camera's not inside it), then add it to the scene
-    cube.position.setZ(-2.5);
     scene.add(cube);
     
     //called once per frame
     function render() {
         window.requestAnimationFrame(render);
-        cube.rotation.x += 0.02;
-        cube.rotation.y += 0.02;
         renderer.render(scene, camera);
     }
     render();
+    
+    //window resize handler
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        render();
+    }
+    window.addEventListener('resize', onWindowResize, false);
 }());
